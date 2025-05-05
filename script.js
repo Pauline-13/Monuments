7b042be0326b3952ad0a64645b2770bd49486d41
Pauline-13 <pauline.tincq@yahoo.fr>
2025-05-05 16:33:49 +0200

    début création constantes JS

---
+++ script.js
@@ -0,0 +1,19 @@
+// 1. Faire des const pour chaque Nom, Pays, Ville, Description ... 
+
+const monuments = {
+    premierMonument: {
+        name: "Exemple1",
+        city: "Ville1",
+        country: "France",
+        description: "lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+        imgUrl: "images/carte_france.webp",
+    },
+
+    deuxiemeMonument: {
+        name: "Exemple2",
+        city: "Ville2",
+        country: "France",
+        description: "lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+        imgUrl: "images/carte_france.webp",
+    }
+};
