e716fcc2cfcd9e8f4ba12c1f1acf3dac858272a8
Pauline T <Pauline-13>
2025-05-06 16:33:20 +0200

    Ajout des photos et descriptions, reste 2 à faire

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const monuments = {
     premierMonument: {
-        name: "Exemple1",
-        city: "Ville1",
+        name: "Tour Eiffel",
+        city: "Paris",
         country: "France",
-        description: "lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+        description: "La Tour Eiffel est l'un des monuments les plus célèbres au monde, située à Paris, sur le Champ de Mars. Conçue par l'ingénieur Gustave Eiffel pour l'Exposition Universelle de 1889, elle mesure 330 mètres de hauteur et fut, jusqu'en 1930, la plus haute structure du monde. Initialement critiquée pour son apparence, elle est aujourd'hui un symbole incontesté de la France et de la capitale parisienne. Chaque année, des millions de visiteurs montent ses étages pour admirer une vue imprenable sur Paris. La Tour Eiffel, illuminée la nuit, est un spectacle à part entière, fascinant les touristes comme les Parisiens.",
         imgUrl: "images/carte_france.webp",
     },
 
     deuxiemeMonument: {
-        name: "Exemple2",
-        city: "Ville2",
+        name: "Pont du Gard",
+        city: "Nîmes",
         country: "France",
-        description: "lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+        description: "Cet Aqueduc romain impressionnant, construit au Ier siècle après J.-C., situé près de Nîmes, dans le sud de la France. Il traverse le Gardon sur trois niveaux de arches, mesurant 49 mètres de hauteur, et servait à acheminer l'eau de la rivière. Cet édifice remarquable témoigne de l'ingéniosité des ingénieurs romains de l'époque. Parfaitement conservé, il est un exemple exceptionnel d'architecture et de génie civil antique. Classé au patrimoine mondial de l'UNESCO, il attire des milliers de visiteurs chaque année.",
         imgUrl: "images/carte_france.webp",
     },
 
@@ -24,10 +24,10 @@ const monuments = {
     },
 
     quatriemeMonument: {
-        name: "Exemple4",
-        city: "Ville4",
+        name: "Mont Aiguille",
+        city: "Vercors",
         country: "France",
-        description: "lorem lorem lorem lorem lorem lorem lorem lorem lorem",
+        description: "Le Mont Aiguille est l'un des sommets les plus emblématiques du massif du Vercors, situé dans les Alpes françaises. Culminant à 2 087 mètres d'altitude, il est connu pour sa forme pyramidale unique, entourée de falaises vertigineuses. Ce mont a marqué l’histoire de l’alpinisme, étant l'un des premiers sommets à être grimpé en 1492. Il offre des panoramas spectaculaires sur les montagnes environnantes et la vallée du Trièves. Très prisé des randonneurs et des grimpeurs, le Mont Aiguille est un véritable défi pour les amateurs de nature et de sensations fortes.",
         imgUrl: "images/carte_france.webp",
     },
 
