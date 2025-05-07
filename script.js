8093f3fd48a01e831cf01fc89927d5f911c024db
Pauline T <Pauline-13>
2025-05-07 16:34:46 +0200

    Finitions CSS

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const monuments = {
         city: "Paris",
         country: "France",
         description: "La Tour Eiffel est l'un des monuments les plus célèbres au monde, située à Paris, sur le Champ de Mars. Conçue par l'ingénieur Gustave Eiffel pour l'Exposition Universelle de 1889, elle mesure 330 mètres de hauteur et fut, jusqu'en 1930, la plus haute structure du monde. Initialement critiquée pour son apparence, elle est aujourd'hui un symbole incontesté de la France et de la capitale parisienne. Chaque année, des millions de visiteurs montent ses étages pour admirer une vue imprenable sur Paris. La Tour Eiffel, illuminée la nuit, est un spectacle à part entière, fascinant les touristes comme les Parisiens.",
-        imgUrl: "images/carte_france.webp",
+        imgUrl: "images/Paris.jpg",
     },
 
     deuxiemeMonument: {
@@ -12,15 +12,15 @@ const monuments = {
         city: "Nîmes",
         country: "France",
         description: "Cet Aqueduc romain impressionnant, construit au Ier siècle après J.-C., situé près de Nîmes, dans le sud de la France. Il traverse le Gardon sur trois niveaux de arches, mesurant 49 mètres de hauteur, et servait à acheminer l'eau de la rivière. Cet édifice remarquable témoigne de l'ingéniosité des ingénieurs romains de l'époque. Parfaitement conservé, il est un exemple exceptionnel d'architecture et de génie civil antique. Classé au patrimoine mondial de l'UNESCO, il attire des milliers de visiteurs chaque année.",
-        imgUrl: "images/carte_france.webp",
+        imgUrl: "images/Nîmes.jpg",
     },
 
     troisiemeMonument: {
-        name: "Exemple3",
-        city: "Ville3",
+        name: "Basilique du Sacré-Coeur ",
+        city: "Paris",
         country: "France",
-        description: "lorem lorem lorem lorem lorem lorem lorem lorem lorem",
-        imgUrl: "images/carte_france.webp",
+        description: "La basilique du Sacré-Cœur est un monument emblématique situé au sommet de la butte Montmartre, à Paris. Construite entre 1875 et 1914, elle est dédiée au Sacré-Cœur de Jésus en expiation des péchés commis durant la guerre franco-prussienne. Son style romano-byzantin, avec ses coupoles blanches et son dôme central, la distingue des autres églises parisiennes. L’intérieur abrite l’un des plus grands mosaïques du monde, représentant le Christ en gloire. Depuis son parvis, la vue panoramique sur Paris est l’une des plus spectaculaires de la capitale.",
+        imgUrl: "images/Paris.jpg",
     },
 
     quatriemeMonument: {
@@ -28,15 +28,15 @@ const monuments = {
         city: "Vercors",
         country: "France",
         description: "Le Mont Aiguille est l'un des sommets les plus emblématiques du massif du Vercors, situé dans les Alpes françaises. Culminant à 2 087 mètres d'altitude, il est connu pour sa forme pyramidale unique, entourée de falaises vertigineuses. Ce mont a marqué l’histoire de l’alpinisme, étant l'un des premiers sommets à être grimpé en 1492. Il offre des panoramas spectaculaires sur les montagnes environnantes et la vallée du Trièves. Très prisé des randonneurs et des grimpeurs, le Mont Aiguille est un véritable défi pour les amateurs de nature et de sensations fortes.",
-        imgUrl: "images/carte_france.webp",
+        imgUrl: "images/Vercors.jpg",
     },
 
     cinquiemeMonument: {
-        name: "Exemple5",
-        city: "Ville5",
+        name: "Château de Chenonceau",
+        city: "Loire",
         country: "France",
-        description: "lorem lorem lorem lorem lorem lorem lorem lorem lorem",
-        imgUrl: "images/carte_france.webp",
+        description: "Le château de Chenonceau, surnommé le 'château des Dames', est l’un des plus emblématiques de la vallée de la Loire. Construit au XVIe siècle, il enjambe gracieusement le Cher grâce à sa galerie en arches, unique en son genre. Il doit sa renommée aux femmes qui l’ont façonné, comme Diane de Poitiers et Catherine de Médicis. Son architecture Renaissance, ses jardins raffinés et son décor intérieur riche en œuvres d’art attirent chaque année de nombreux visiteurs. C’est un symbole de l’élégance française et de l’influence féminine dans l’histoire.",
+        imgUrl: "images/Loire.jpg",
     },
 };
 
